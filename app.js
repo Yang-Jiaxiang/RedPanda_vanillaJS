4cba646a1c7be39d6f3c46c31ed8091fbb21cac7
Yang-Jiaxiang <yang900412@gmail.com>
2022-08-31 22:12:29 +0800

    feat：透過header渲染menu

--- app.js
+++ app.js
@@ -1,67 +1,24 @@
-// async function fetchData(url) {
-//     try {
-//         await fetch(url, {
-//             headers: {
-//                 "Accept-Charset": "utf-8",
-//                 Accept: "application/fhir+xml;q=1.0, application/fhir+json;q=1.0, application/xml+fhir;q=0.9,application/json+fhir;q=0.9",
-//                 "User-Agent":
-//                     "HAPI-FHIR/6.0.0-PRE8-SNAPSHOT (FHIR Client; FHIR 4.0.1/R4; apache)",
-//                 "Accept-Encoding": "gzip",
-//             },
-//             mode: "cors",
-//         }).then((response) => {
-//             response.json().then(function (data) {
-//                 document.getElementById("contend").innerHTML =
-//                     data.entry[0].resource.id;
+const fhirServer = "https://hapi.fhir.org/baseR4/";
+const headersJson = "./header.json";
+const Support = "./StructureDefinition-Patient-twcore.json";
 
-//                 console.log(data);
-//             });
-//         });
-//     } catch (error) {
-//         console.log(error);
-//     }
-// }
-// fetchData("https://hapi.fhir.org/baseR4/Patient");
-
-// async function fetchData(url) {
-//     try {
-//         let response = await fetch(url, {
-//             headers: {
-//                 "Accept-Charset": "utf-8",
-//                 Accept: "application/fhir+xml;q=1.0, application/fhir+json;q=1.0, application/xml+fhir;q=0.9,application/json+fhir;q=0.9",
-//                 "User-Agent":
-//                     "HAPI-FHIR/6.0.0-PRE8-SNAPSHOT (FHIR Client; FHIR 4.0.1/R4; apache)",
-//                 "Accept-Encoding": "gzip",
-//             },
-//             mode: "cors",
-//         });
-//         let data = await response.text();
-//         console.log(data);
-//     } catch (error) {
-//         console.log(error);
-//     }
-// }
-// fetchData("https://hapi.fhir.org/baseR4/Patient");
-
-async function loadIntoTable(url, header, Support, resource, table) {
-    resource = resource.toLowerCase();
+async function loadIntoTable(resource, table) {
+    //resource = resource.toLowerCase();
     const tableHead = table.querySelector("thead");
     const tableBody = table.querySelector("tbody");
-    const responseData = await fetch(url);
-    const responseHeader = await fetch(header);
+    const responseData = await fetch(fhirServer + resource);
+    const responseHeader = await fetch(headersJson);
     const responseSupport = await fetch(Support);
 
     const { entry } = await responseData.json();
     const { headers } = await responseHeader.json();
     const { differential } = await responseSupport.json();
-
     // differential.element.map((item) => {
     //     item.mustSupport && console.log(item.id);
     // });
-
+    
     tableHead.innerHTML = "<tr></tr>";
     tableHead.tableBody = "";
-
     //產生header
     for (const headerText of headers[resource]) {
         const headerElement = document.createElement("th");
@@ -86,7 +43,7 @@ async function loadIntoTable(url, header, Support, resource, table) {
             if (typeof row.resource[header] === "string") {
                 cellElement.textContent = row.resource[header];
             } else if (typeof row.resource[header] === "object") {
-                const html = objectToString(row.resource[header]);
+                const html = row.resource[header]; //objectToString(row.resource[header]);
                 cellElement.innerHTML = html;
             }
             rowElement.appendChild(cellElement);
@@ -117,7 +74,6 @@ function objectToString(jsons) {
 
 //下方展開列
 function expandOpen(expand, row, headers, resource) {
-    console.log(row);
     function ExpanRender() {
         var html = `<button onclick="copyButtonFunction()">Copy Json</button>
         <a id="exportJSON" onclick="downJsonButton(this);" class="btn" download>Download Json</a>
@@ -170,15 +126,24 @@ function downJsonButton(el) {
 
     //console.log();
 
-    const filename = `${JSON.parse(copyText.value).resource.resourceType}_${JSON.parse(copyText.value).resource.id}.json`;
+    const filename = `${JSON.parse(copyText.value).resource.resourceType}_${
+        JSON.parse(copyText.value).resource.id
+    }.json`;
     el.setAttribute("href", "data:" + data);
     el.setAttribute("download", `${filename}`);
 }
 
-loadIntoTable(
-    "https://hapi.fhir.org/baseR4/Patient/",
-    "./header.json",
-    "./StructureDefinition-Patient-twcore.json",
-    "Patient",
-    document.querySelector("table")
-);
+//渲染MenuBar
+async function menuBar(header) {
+    const menuBarDiv = document.querySelector(".meunBar");
+    const responseHeader = await fetch(header);
+    const { headers } = await responseHeader.json();
+    const JsonArray = Object.entries(headers);
+    menuBarDiv.innerHTML = `<li><a class="active">Resource</a></li>`;
+
+    for (const header of JsonArray) {
+        menuBarDiv.innerHTML += `<li><a onclick={loadIntoTable(loadIntoTable("${header[0]}",document.querySelector("table")))}>${header[0]}</a></li>`;
+    }
+}
+
+menuBar("./header.json");
